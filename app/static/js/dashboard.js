5e28eed519dde7a50f719763f761fc0cb57e2528
Steve Barskey <barskey@gmail.com>
2016-06-29 23:49:47 -0700

    Lots of updates. Switched to two navbars at top. Fiex missing /div in dashboard.html. Added icon for sign-in/out.:

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,10 +1,3 @@
 $( "#test1" ).draggable({ grid: [10, 10 ], containment: "parent" });
-$( ".draggable-trigger" ).draggable();
-$( ".droppable-trigger" ).droppable({
-  accept: ".draggable-trigger",
-  activeClass: "ui-state-default",
-  drop: function( event, ui ) {
-    $( this )
-      .addClass( "ui-state-highlight" )
-    }
-});
+$( ".sortable" ).sortable();
+$( ".sortable" ).disableSelection();
