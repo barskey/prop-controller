e05e0c8a50873a71110204bb6337385a1b8d23e3
Steve Barskey <barskey@gmail.com>
2016-07-02 20:42:27 -0700

    Updates to dashboard.* files

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,4 +1,14 @@
-$( '[data-toggle="popover"]' ).popover();
-$( "#test1" ).draggable({ grid: [10, 10 ], containment: "parent" });
+function createEvent(event) {
+  event.preventDefault();
+  $( ".dashboard" ).append("div");
+};
+$( '[data-toggle="popover"]' ).popover({
+  title: "Outputs",
+  content: "Controller 1<div class='btn-group' role='group'><button type='button' class='btn btn-warning'>A</button><button type='button' class='btn btn-warning'>B</button><button type='button' class='btn btn-warning'>C</button><button type='button' class='btn btn-warning'>D</button></div>",
+  html: true,
+  placement: "right",
+  container: "#event-1"
+});
+$( ".draggable-panel" ).draggable({ grid: [10, 10 ], containment: "parent" });
 $( ".sortable" ).sortable();
 $( ".sortable" ).disableSelection();
