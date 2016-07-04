25c6a82ae7378ee861d0f363d961a6a00850add9
Steve Barskey <barskey@gmail.com>
2016-07-03 23:58:03 -0700

    Add trigger works. Add action not working. Add Event not working. Need to configure input ports for triggers.

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,3 +1,4 @@
+$( '[data-toggle="tooltip"]' ).tooltip();
 function createEvent(event) {
   event.preventDefault();
   $( ".dashboard" ).append("div");
@@ -12,3 +13,68 @@ $( '[data-toggle="popover"]' ).popover({
 $( ".draggable-panel" ).draggable({ grid: [10, 10 ], containment: "parent" });
 $( ".sortable" ).sortable();
 $( ".sortable" ).disableSelection();
+$( ".add-trigger" ).click(function() {
+  $( "#addTriggerModal" ).modal("toggle", $(this));
+});
+$( ".add-action" ).click(function() {
+  $( "#addActionModal" ).modal("toggle", $(this));
+});
+$('#addTriggerModal').on('show.bs.modal', function (event) {
+  var obj = $(event.relatedTarget); // object that triggered the modal
+  var eventid = obj.data('eventid'); // Extract info from data-* attributes
+  var modal = $(this);
+  modal.find( '#triggerEventnameSelect' ).val(eventid);
+})
+$('#addActionModal').on('show.bs.modal', function (event) {
+  var obj = $(event.relatedTarget); // object that triggered the modal
+  var eventid = obj.data('eventid'); // Extract info from data-* attributes
+  var modal = $(this);
+  modal.find( '#actionEventnameSelect' ).val(eventid);
+})
+$( "#triggerType" ).change(function() {
+  $( ".triggerForm" ).addClass("hidden");
+  var trigger = $( this ).find( ":selected" ).text();
+  $( "#" + trigger.replace(/\s/g, '') ).removeClass("hidden");
+});
+$( "#actionType" ).change(function() {
+  $( ".actionForm" ).addClass("hidden");
+  var action = $( this ).find( ":selected" ).text().replace(/\s/g, '');
+  $( "#" + action ).removeClass("hidden");
+});
+
+$( "#triggerModalAddButton" ).click(function() {
+  var $btn = $( this ).button("Adding...");
+  var $thiseventid = $( "#triggerModalForm" ).find( "#triggerEventnameSelect option:selected" ).val();
+  var $triggerid = $( "#triggerModalForm" ).find( "#triggerType option:selected" ).val();
+  var $triggername = $( "#triggerModalForm" ).find( "#triggerType option:selected" ).text();
+  $.ajax({
+    url: "/add_trigger_to_event",
+    data: $( "#triggerModalForm" ).serialize(),
+    type: "POST",
+    dataType: "json",
+    success: function( response ) {
+      var $triggerlist = $( "#" + $thiseventid ).find( ".trigger-list" )
+      $triggerlist.empty();
+      $.each( response.triggers, function( index, value ) {
+        //alert(value.triggerid + ": " + value.triggerparam);
+        $triggerlist.append(
+          $( "<li>" ).addClass( "list-group-item" ).addClass( "list-group-item-warning" ).attr( "id", "trigger-" + value.triggerid ).append(
+            $( "<span>" ).addClass( "glyphicon" ).addClass( "glyphicon-menu-hamburger" )
+          ).append(
+            $( "<span>" ).append( "&nbsp;" + value.triggername + "&nbsp;" )
+          ).append(
+            $( "<div>" ).addClass( "pull-right" ).attr( { "data-toggle":"tooltip", "data-placement":"right", "title":value.tiptitle, "data-container":"body", "data-html":"true" } ).append(
+              $( "<span>" ).addClass( "glyphicon" ).addClass( "glyphicon glyphicon-info-sign" )
+            )
+          )
+        )
+      });
+      $btn.button("reset");
+      $( "#addTriggerModal" ).modal("toggle");
+      $( '[data-toggle="tooltip"]' ).tooltip();
+    },
+      error: function( error ) {
+      console.log(error);
+    }
+  });
+});
--- app/views.py
+++ app/views.py
@@ -1,8 +1,74 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, jsonify
+
+#Dummy data for testing
+triggers = [
+    {'triggerid': '1', 'triggername': 'Motion'},
+    {'triggerid': '2', 'triggername': 'Pushbutton'},
+    {'triggerid': '3', 'triggername': 'Switch'},
+    {'triggerid': '4', 'triggername': 'Event'}
+]
+actions = [
+    {'actionid': '1', 'actionname': 'Turn On'},
+    {'actionid': '2', 'actionname': 'Turn Off'},
+    {'actionid': '3', 'actionname': 'Toggle'},
+    {'actionid': '4', 'actionname': 'Blink'},
+    {'actionid': '5', 'actionname': 'Play Sound'}
+]
+sounds = [
+    {'soundid': '1', 'soundname': 'Scream'},
+    {'soundid': '2', 'soundname': 'Zombie'},
+    {'soundid': '3', 'soundname': 'Evil Laugh'},
+    {'soundid': '4', 'soundname': 'Ghost'}
+]
+events = [
+    {'eventid': '1', 'eventname': 'Driveway Motion', 'triggers':[
+        {'triggerid': '1', 'triggername': 'Motion', 'triggerparam': '30000', 'tiptitle':'Reset after: 30000ms<br />(click to edit)'}
+    ], 'actions':[
+        {'actionid': '1', 'actionparam': '1000'},
+        {'actionid': '2', 'actionparam': '3000'}
+    ]},
+    {'eventid': '2', 'eventname': 'Sidewalk Motion', 'triggers':[
+        {'triggerid': '1', 'triggername': 'Motion', 'triggerparam': '10000', 'tiptitle':'Reset after: 10000ms<br />(click to edit)'}
+    ], 'actions':[
+        {'actionid': '4', 'actionparam': '2000'}
+    ]}
+]
 
 @app.route('/')
 @app.route('/dashboard')
 def dashboard():
     projectname = "Halloween 2016"
-    return render_template('dashboard.html', title='Dashboard', projectname=projectname)
+    return render_template('dashboard.html', title='Dashboard', projectname=projectname, triggers=triggers, actions=actions, sounds=sounds, events=events)
+
+@app.route('/add_trigger_to_event', methods=['POST'])
+def add_trigger_to_event():
+    param = ""
+    tiptitle = ""
+    a, eventid = request.form['triggerEventnameSelect'].split("-")
+    triggerid = request.form['triggerType']
+    triggername = ""
+    for t in triggers:
+        if t['triggerid'] == triggerid:
+            triggername = t['triggername']
+    if triggerid == '1':
+        param = request.form['resetTime']
+        tiptitle = "Reset after: " + param + "ms<br />(click to edit)"
+    elif triggerid == '2':
+        param = request.form['defaultState']
+    elif triggerid == '3':
+        tiptitle = "No parameters to config"
+        tiptitle = "Default state: " + param + "<br />(click to edit)"
+    elif triggerid == '4':
+        param = request.form['eventTrigger']
+        tiptitle = "Using event: " + param + "<br />(click to edit)"
+    #Add to sql db and commit, return new list of actions
+    for e in events:
+        if e['eventid'] == eventid:
+            e['triggers'].append({'triggerid': triggerid, 'triggername': triggername, 'triggerparam': param, 'tiptitle': tiptitle})
+            return jsonify(triggers = e['triggers'])
+
+@app.route('/testpost', methods=['POST'])
+def testpost():
+	items = request.form
+	return render_template('testpost.html', items=items)
